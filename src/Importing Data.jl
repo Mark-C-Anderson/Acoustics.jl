ed807073b83b246eb5d6b78bb369fe42eec0befb
Mark <anderson.mark.az@gmail.com>
2021-06-10 17:15:57 -0600

    binfileload with args

--- src/Importing Data.jl
+++ src/Importing Data.jl
@@ -1,5 +1,13 @@
-function binfileload(filename)
+function binfileload(path::String,IDnum::Int,CHnum::Int)
     
+    # Zero-padding the ID and CH numbers
+    IDnum = lpad(IDnum,3,"0")
+    CHnum = lpad(CHnum,3,"0")
+    IDname = "ID"
+    CHname = ""
+
+    filename = string(path,"/",IDname,IDnum,"_",CHname,CHnum,".bin")
+
     io = open(filename,"r")
     
     fileLength = Int(length(read(io))/4)
--- test/demo.jl
+++ test/demo.jl
@@ -1,9 +1,14 @@
 using Pkg
 using Plots
+using Revise
 import Acoustics: binfileload
 
-x = binfileload("/Users/markanderson/Desktop/ID011_002.bin")
+x = binfileload("/Users/markanderson/Desktop",11,2)
 fs = 51.2e3;
 dt = 1/fs;
 t = 0:dt:length(x)*dt-dt;
-plot(t,x)
+plot(t,x,
+    label = "Waveform",
+    title = "Falcon 9 Landing",
+    xlabel = "Time (s)",
+    ylabel = "Pressure (Pa)")
